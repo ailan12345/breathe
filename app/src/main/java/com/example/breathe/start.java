c997110e8c708d9eba5e10cc6a766922aff85afd
ailan12345 <karta2599434@gmail.com>
2019-03-25 13:06:20 +0800

    045 good

--- app/src/main/java/com/example/breathe/start.java
+++ app/src/main/java/com/example/breathe/start.java
@@ -1,5 +1,8 @@
 package com.example.breathe;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.app.Application;
 import android.app.Service;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -9,10 +12,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+
+import java.util.Timer;
 
 public class start extends AppCompatActivity {
 
@@ -21,6 +30,8 @@ public class start extends AppCompatActivity {
         myVibrator.vibrate(time);
     }
 
+    CountDownTimer yourCountDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,30 +39,55 @@ public class start extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent intent = this.getIntent();
-        int reciprocal = intent.getIntExtra("reciprocal", 10000);
+        ImageView image = (ImageView) findViewById(R.id.image);
+//        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.fadescalecombine);
+//        image.startAnimation(hyperspaceJump);
+//        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+//                R.animator.breath);
+//        set.setTarget(image);
+//        set.start();
+//        TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
+//        Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.fadescalecombine);
+//        myAwesomeTextView.startAnimation(scaleAnimation);
 
-        ImageView gifImageView2 = (ImageView) findViewById(R.id.imageView);
 
-        new CountDownTimer(reciprocal, 1000) {
-            TextView mTextView = (TextView) findViewById(R.id.textView6);
-            @Override
-            public void onTick(long millisUntilFinished) {
-                //倒數秒數中要做的事
-//                        mTextView.setText("倒數時間:" + millisUntilFinished / 1000);
-                TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
-                mTextView.setText("倒數時間:" + millisUntilFinished/1000);
-                myAwesomeTextView.setText("aaa" + ((millisUntilFinished/1000)+1)%6);
-                if(((millisUntilFinished/1000)+1)%6 == 0){
-                    setVibrate(400); // 震動 0.4 秒
-                }
+
+//        ImageView gifImageView2 = (ImageView) findViewById(R.id.imageView);
+//        yourCountDownTimer;
+    Intent intent = this.getIntent();
+    int reciprocal = intent.getIntExtra("reciprocal", 10000);
+    yourCountDownTimer = new CountDownTimer(reciprocal, 1000) {
+        TextView mTextView = (TextView) findViewById(R.id.textView6);
+        int i = 0;
+        @Override
+        public void onTick(long millisUntilFinished) {
+            //倒數秒數中要做的事
+            TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
+            mTextView.setText("倒數時間:" + millisUntilFinished/1000 + "秒");
+            if(i%2 == 1){
+                myAwesomeTextView.setText("吸氣" + ((millisUntilFinished/1000))%6);
+            }else{
+                myAwesomeTextView.setText("吐氣" + ((millisUntilFinished/1000))%6);
             }
-            @Override
-            public void onFinish() {
-                //倒數完成後要做的事
-                mTextView.setText("Done!");
+
+            if(((millisUntilFinished/1000))%6 == 0){
+                setVibrate(200); // 震動 0.4 秒
+                i += 1;
             }
-        }.start();
+        }
+        @Override
+        public void onFinish() {
+            //倒數完成後要做的事
+            mTextView.setText("Done!");
+        }
+    }.start();
     }
-
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Toast.makeText(start.this,
+                "test" , Toast.LENGTH_SHORT).show();
+        yourCountDownTimer.cancel();
+    }
 }
