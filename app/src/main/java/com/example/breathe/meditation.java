0789320e034d92ef5b33c92cc796f582513f6cb1
ailan12345 <karta2599434@gmail.com>
2019-03-25 11:27:17 +0800

    3 CountDownTimer

--- app/src/main/java/com/example/breathe/meditation.java
+++ app/src/main/java/com/example/breathe/meditation.java
@@ -39,22 +39,23 @@ public class meditation extends AppCompatActivity {
         myVibrator.vibrate(time);
     }
     public void addListenerOnButton() {
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         btnDisplay = (Button) findViewById(R.id.button5);
         btnDisplay.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+                RadioGroup radioGroup2 = (RadioGroup) findViewById(R.id.radioGroup2);
                 // get selected radio button from radioGroup
                 int selectedId = radioGroup.getCheckedRadioButtonId();
                 // find the radiobutton by returned id
-                radioButton = (RadioButton) findViewById(selectedId);
+                RadioButton radioButton = (RadioButton) findViewById(selectedId);
 
                 Toast.makeText(meditation.this,
                         "準備計時" + radioButton.getText(), Toast.LENGTH_SHORT).show();
-                int reciprocal = 10000;
+                int reciprocal = 10000; //時數
                 switch(radioButton.getId()){
                     case R.id.radioButton2:
-                        reciprocal = 60000;
+                        reciprocal = 20000;
                         break;
                     case R.id.radioButton3:
                         reciprocal = 120000;
@@ -66,9 +67,30 @@ public class meditation extends AppCompatActivity {
                         reciprocal = 240000;
                         break;
                 }
+
+                // get selected radio button from radioGroup
+                int selectedId2 = radioGroup2.getCheckedRadioButtonId();
+                // find the radiobutton by returned id
+                RadioButton radioButton2 = (RadioButton) findViewById(selectedId2);
+
+                int frequency = 6; //頻率
+                switch(radioButton2.getId()){
+                    case R.id.radioButton6:
+                        frequency = 6;
+                        break;
+                    case R.id.radioButton7:
+                        frequency = 8;
+                        break;
+                    case R.id.radioButton8:
+                        frequency = 10;
+                        break;
+                }
+
+
                 Intent gostart = new Intent();
                 gostart.setClass(meditation.this  , start.class);
                 gostart .putExtra("reciprocal",reciprocal);
+                gostart .putExtra("frequency",frequency);
                 startActivity(gostart);
 
             }
--- app/src/main/java/com/example/breathe/start.java
+++ app/src/main/java/com/example/breathe/start.java
@@ -31,6 +31,9 @@ public class start extends AppCompatActivity {
     }
 
     CountDownTimer yourCountDownTimer;
+    CountDownTimer yourCountDownTimer2;
+    int reciprocal = 10000;
+    int frequency = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,30 +53,17 @@ public class start extends AppCompatActivity {
 //        Animation scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.fadescalecombine);
 //        myAwesomeTextView.startAnimation(scaleAnimation);
 
-
-
 //        ImageView gifImageView2 = (ImageView) findViewById(R.id.imageView);
 //        yourCountDownTimer;
     Intent intent = this.getIntent();
-    int reciprocal = intent.getIntExtra("reciprocal", 10000);
+    reciprocal = intent.getIntExtra("reciprocal", 10000); //總進行時數
+    frequency = intent.getIntExtra("frequency", 2); //頻率
     yourCountDownTimer = new CountDownTimer(reciprocal, 1000) {
         TextView mTextView = (TextView) findViewById(R.id.textView6);
-        int i = 0;
         @Override
         public void onTick(long millisUntilFinished) {
             //倒數秒數中要做的事
-            TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
-            mTextView.setText("倒數時間:" + millisUntilFinished/1000 + "秒");
-            if(i%2 == 1){
-                myAwesomeTextView.setText("吸氣" + ((millisUntilFinished/1000))%6);
-            }else{
-                myAwesomeTextView.setText("吐氣" + ((millisUntilFinished/1000))%6);
-            }
-
-            if(((millisUntilFinished/1000))%6 == 0){
-                setVibrate(200); // 震動 0.4 秒
-                i += 1;
-            }
+            mTextView.setText("倒數時間:" + millisUntilFinished/1000 + " (秒)");
         }
         @Override
         public void onFinish() {
@@ -81,6 +71,38 @@ public class start extends AppCompatActivity {
             mTextView.setText("Done!");
         }
     }.start();
+
+        yourCountDownTimer2 = new CountDownTimer(reciprocal, (frequency-1)*1000) {
+            int i = 0;
+            int frequencyTime = 0;
+            TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //倒數秒數中要做的事
+                setVibrate(200); // 震動 0.4 秒
+                i += 1;
+                new CountDownTimer(frequency*1000, 1000) {
+                    TextView mTextView = (TextView) findViewById(R.id.textView6);
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        if (i % 2 == 1) {
+                            myAwesomeTextView.setText("吸氣" + ((millisUntilFinished / 1000)));
+                        } else {
+                            myAwesomeTextView.setText("吐吐氣" + ((millisUntilFinished / 1000)));
+                        }
+                    } @Override
+                    public void onFinish() {
+                        //倒數完成後要做的事
+                    }
+                }.start();
+            }
+            @Override
+            public void onFinish() {
+                //倒數完成後要做的事
+                setVibrate(200); // 震動 0.4 秒
+                myAwesomeTextView.setText("Done!");
+            }
+        }.start();
     }
     @Override
     public void onPause()
@@ -89,5 +111,6 @@ public class start extends AppCompatActivity {
         Toast.makeText(start.this,
                 "test" , Toast.LENGTH_SHORT).show();
         yourCountDownTimer.cancel();
+        yourCountDownTimer2.cancel();
     }
 }
